f67386392f49b2a9b03d4eedbfc53e6769be6c52
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-03-26 10:47:21 -0600

    Update diagrama_1a.c

--- diagrama_1a.c
+++ diagrama_1a.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void diagramaA(){
 int n;
 scanf("%d", &n);
 
@@ -10,6 +10,17 @@ printf("positivo\n");}
 
 else {
 printf("negativo\n");}
+
+return 0;
+}
+
+void diagramaB(){
+int n1;
+scanf("%d", &n1);
+if(n%2 == 0){
+printf(" es par\n");}
+else{
+printf("no es par");}
 
 return 0;
 }
