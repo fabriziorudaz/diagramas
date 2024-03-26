1ff21ff183b89d6dda87c72496c9adc4ad02a16d
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-03-26 10:51:28 -0600

    Update diagrama_1c.c

--- diagrama_1c.c
+++ diagrama_1c.c
@@ -4,17 +4,15 @@ int main(){
 int n;
 scanf("%d",&n);
 
-switch (n)
-{
-case 1: if(n%2 == 0){
-printf("es divisible por 2");};
-break;
-case 2: if(n%3 == 0){
-printf ("es divisible por 3");};
-break;
-default:
-printf ("no es divisible por ninguno");
+if(n % 2 ==0){
+  printf("es divisible por 2");
 }
-
+  if else (n % 3==0){
+    printf("es divisible por 3");
+  }
+  else {
+    printf("no es divisible por ninguno");
+  }
+  
 return 0;
 }
